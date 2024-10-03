e43134f5282e0c706db31c4056b895cb869d41fb
Sham Rai <0rais10@solent.ac.uk>
2024-10-03 14:47:57 +0100

    Added code to demonstrate the use of a logical or operator

--- Week 2/nested_if.py
+++ Week 2/nested_if.py
@@ -8,3 +8,25 @@ elif cover == "hard":
 else:
     print("Soft covers with coils or stiches are great for short books")
 
+response = input("Where should I look")
+if response == "in the bedroom":
+    bedroom = input("Where in the bedroom should I look?")
+    if bedroom == "under the bed":
+      print("Found some shoes but no phone")
+    else:
+        print("Found some mess but no phone")
+elif response == "in the bathroom":
+  bathroom = input("Where in the bathroom should I look?")
+  if bathroom == "in the bathtub":
+      print("Found a rubber duck but no phone")
+  else:
+      print("Found bathroom stuff but no phone")
+elif response == "in the living room":
+    living_room = input("Where in the living room should I look?")
+    if living_room == "on the table":
+        print("Yes! I found my phone!")
+    else:
+        print("Found some stuff but not my phone")
+else:
+    print("I do not know where that is but I will keep looking")
+
