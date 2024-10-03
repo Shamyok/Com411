161ffa88d6519009a8396b3b3129c57cc401e2c4
Sham Rai <0rais10@solent.ac.uk>
2024-10-03 13:17:51 +0100

    Added code to demonstrate the use of an if...elif...else statement

--- Week 2/If_statements.py
+++ Week 2/If_statements.py
@@ -16,3 +16,14 @@ else:
 
 print("Activity completed!")
 
+print("Towards which direction should I go (up, down ,left or right) up?")
+direction = input().lower()
+
+if direction == "up":
+    print("I am moving in an upwards direction!")
+elif direction == "down":
+    print("I am moving in an downwards direction!")
+elif direction == "left":
+    print("I am moving to the left!")
+elif direction == "right":
+    print("I am moving to the right!")
