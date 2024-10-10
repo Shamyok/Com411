a36f401e83611b715384a86249236f65579d7df4
Sham Rai <0rais10@solent.ac.uk>
2024-10-10 14:20:59 +0100

    Added code to demonstrate the use of a simple for loop.

--- Week 3/while_loops.py
+++ Week 3/while_loops.py
@@ -1,33 +1,33 @@
-#apples = 0
-#apples = int(input("How many apples should I remove?"))
+apples = 0
+apples = int(input("How many apples should I remove?"))
 
-#for i in range(apples):
-#  print("Removed apple")
+for i in range(apples):
+  print("Removed apple")
 
-#obstacles = 0
-#obstacles = int(input("How many obstacles should I avoid?"))
-#while obstacles >0:
-#  print(f"Avoiding...Done!{obstacles} obstacles avoided.")
-#  obstacles -= 1
-#print("All obstacles have been avoided")
+obstacles = 0
+obstacles = int(input("How many obstacles should I avoid?"))
+while obstacles >0:
+  print(f"Avoiding...Done!{obstacles} obstacles avoided.")
+  obstacles -= 1
+print("All obstacles have been avoided")
 
-#i = 1
-#bar = int(input("How many bars should be charged?"))
-#while i <=bar:
-#  print(f"Charging:{'█'*i}")
-#  i +=1
-#print("The battery is fully charged")
+i = 1
+bar = int(input("How many bars should be charged?"))
+while i <=bar:
+  print(f"Charging:{'█'*i}")
+  i +=1
+print("The battery is fully charged")
 
-#phrase = (input("Please type a phrase"))
-#x = len(phrase)
-#print("hi "*x)
+phrase = (input("Please type a phrase"))
+x = len(phrase)
+print("hi "*x)
 
-#print("Calculating the sum of the first 100 numbers...")
-#num = 100
-#sum = 0
-#for i in range(num+1):
-#  sum += i
-#print(f"...Done! The answer is {sum}.")
+print("Calculating the sum of the first 100 numbers...")
+num = 100
+sum = 0
+for i in range(num+1):
+  sum += i
+print(f"...Done! The answer is {sum}.")
 
 tally = 0
 i=1
@@ -37,3 +37,4 @@ while i <= num:
     tally += answer
     i +=1
 print(tally)
+
