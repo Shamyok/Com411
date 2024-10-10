a73ed20ae38f30b59461c65c2a552fa044d411c8
Sham Rai <0rais10@solent.ac.uk>
2024-10-10 13:36:00 +0100

    Added code to demonstrate the use of a while loop with ASCII art

--- Week 3/while_loops.py
+++ Week 3/while_loops.py
@@ -10,3 +10,11 @@ while obstacles >0:
   print(f"Avoiding...Done!{obstacles} obstacles avoided.")
   obstacles -= 1
 print("All obstacles have been avoided")
+
+i = 1
+bar = int(input("How many bars should be charged?"))
+while i <=bar:
+  print(f"Charging:{'█'*i}")
+  i +=1
+print("The battery is fully charged")
+
