66f23f8aba5e798045585ed8993f0d8e88a4166e
Sham Rai <0rais10@solent.ac.uk>
2024-10-24 13:52:02 +0100

    Added code to demonstrate how to index a list.

--- Week 5/list_tasks.py
+++ Week 5/list_tasks.py
@@ -2,3 +2,8 @@ def directions():
     print(list(["Move forward", "Move backward", "Turn left", "Turn right"]))
 
 directions()
+
+path = ["Move forward", 10, "Move backward", 5 , "Move Left", 3 , "Move Right", 1]
+print("Moving...")
+for i in range(0, len(path), 2):
+    print(f"{path[i]} for {path[i+1]}")
