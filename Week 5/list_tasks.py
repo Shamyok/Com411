1efce4cf6f4ead22ee062f6b264c4ae7926bb506
Shamyok <shamyokrai@gmail.com>
2024-11-02 15:58:44 +0000

    Added code to demonstrate how to iterate a list

--- Week 5/list_tasks.py
+++ Week 5/list_tasks.py
@@ -1,6 +1,6 @@
 def directions():
     steps: list = ["Move forward", "Move backward", "Turn left", "Turn right"]
-    print(steps)
+    return steps
 
 def run_task1():
     directions()
@@ -15,5 +15,14 @@ def run_task2():
     for i in range(0, len(movements1), 2):
         print(f"{movements1[i]} for {movements1[i+1]}")
 
+def menu():
+    print("Please select a direction")
+    direction_list = directions()
+    for i in range(len(direction_list)):
+        direction = direction_list[i]
+        print(f"{i}: {direction}")
 
+def run_task3():
+    menu()
 
+run_task3()
