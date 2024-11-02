f7146b7bc63057d3998e64ba6d4c410309a703d9
Shamyok <shamyokrai@gmail.com>
2024-11-02 09:21:25 +0000

    Added code to demonstrate the import and use of a module.

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -2,8 +2,6 @@ def listen():
     sound = input("What sound did you hear?")
     print(f"That was a loud {sound}!")
 
-
-
 def identify():
     reponse = input("What lies before us?")
     if reponse.lower() == "a large boulder":
---
+++ Week 4/module_tasks.py
@@ -0,0 +1,20 @@
+import random
+
+min_value = int(input("Please enter the minimum value: "))
+max_value = int(input("Please enter the maximum value: "))
+
+ran_num = random.randint(min_value, max_value)
+print(ran_num)
+user_guess = int(input(f"I am thinking of a number between {min_value} and {max_value}. Can you guess what it is?"))
+
+while user_guess != ran_num:
+    if user_guess > ran_num:
+        print("Your guess is too high.")
+        user_guess = int(input("Try again"))
+
+    elif user_guess < ran_num:
+        print("Your guess is too low.")
+        user_guess = int(input("Try again"))
+
+if user_guess == ran_num:
+        print("Congrats! You guessed my number!")
