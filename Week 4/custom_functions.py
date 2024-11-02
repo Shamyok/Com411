7b1488f762784425af5a8d7acd3038c495a0ffc1
Shamyok <shamyokrai@gmail.com>
2024-11-02 00:28:35 +0000

    Added code to demonstrate the use of a user-defined function with a parameter.

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -2,7 +2,7 @@ def listen():
     sound = input("What sound did you hear?")
     print(f"That was a loud {sound}!")
 
-listen()
+
 
 def identify():
     reponse = input("What lies before us?")
@@ -10,7 +10,17 @@ def identify():
         print("Its time to run!")
     else:
         print("We will be fine.")
-identify()
+
 
 def escape_by(plan):
-    if plan == "jumping over"
+    if plan == "jumping over":
+        print("We cannot esacpe that way! The boulder is too big!")
+    elif plan == "running around":
+        print("We cannot escape that way! The boulder is moving too fast!")
+    elif plan == "cross bridge ahead":
+        print("That might just work! Lets go!")
+    else:
+        print("We cannot escape that way! The boulder is in the way!")
+escape_by("jumping over")
+escape_by("running around")
+escape_by("cross bridge ahead")
--- Week 5/list_tasks.py
+++ Week 5/list_tasks.py
@@ -1,10 +1,10 @@
 def directions():
     print(list(["Move forward", "Move backward", "Turn left", "Turn right"]))
 
-#path = ["Move forward", 10, "Move backward", 5 , "Move Left", 3 , "Move Right", 1]
-#print("Moving...")
-#for i in range(0, len(path), 2):
-#    print(f"{path[i]} for {path[i+1]}")
+path = ["Move forward", 10, "Move backward", 5 , "Move Left", 3 , "Move Right", 1]
+print("Moving...")
+for i in range(0, len(path), 2):
+    print(f"{path[i]} for {path[i+1]}")
 
 
 steps = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
