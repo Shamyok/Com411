9c6188592dc617a8951ca53495712276f44ea607
Shamyok <shamyokrai@gmail.com>
2024-11-02 11:25:49 +0000

    Added code to demonstrate how to index a list

--- Week 5/list_tasks.py
+++ Week 5/list_tasks.py
@@ -1,28 +1,19 @@
 def directions():
-    print(list(["Move forward", "Move backward", "Turn left", "Turn right"]))
+    steps: list = ["Move forward", "Move backward", "Turn left", "Turn right"]
+    print(steps)
 
-path = ["Move forward", 10, "Move backward", 5 , "Move Left", 3 , "Move Right", 1]
-print("Moving...")
-for i in range(0, len(path), 2):
-    print(f"{path[i]} for {path[i+1]}")
+def run_task1():
+    directions()
 
+def movements():
+    path = ["Move Forward", 10, "Move backward", 5,"Move Left",3, "Move Right", 1]
+    return path
 
-steps = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
-
-def menu():
-    print("Please select a direction:")
-    for i in range(len(steps)):
-        print(f"{i}: {steps[i]}")
-
-def menu_and_input():
-    print("Please select a direction:")
-    for i in range(len(steps)):
-        print(f"{i}: {steps[i]}")
-    escape = input()
-menu_and_input()
-
-
-
+def run_task2():
+    print("Moving...")
+    movements1 = movements()
+    for i in range(0, len(movements1), 2):
+        print(f"{movements1[i]} for {movements1[i+1]}")
 
 
 
