8632a66c401e6b8fef535ae2e21527946e47c8e6
Sham Rai <0rais10@solent.ac.uk>
2024-11-14 13:42:25 +0000

    Added code to display multiple plots

--- week8/pllots.py
+++ week8/pllots.py
@@ -9,3 +9,32 @@ def run_task1():
     y_values = [1,4,9,16,25]
     display_line(x_values,y_values)
 
+def small():
+    x = [3,3,4,4,3]
+    y = [3,4,4,3,3]
+    plt.xlabel("x values")
+    plt.ylabel("y values")
+
+    plt.plot(x,y,"ro--")
+
+
+def medium():
+    x = [2,2,5,5,2]
+    y = [2,5,5,2,2]
+    plt.xlabel("x values")
+    plt.ylabel("y values")
+    plt.plot(x,y,"gs--")
+
+def large():
+    x = [1,1,6,6,1]
+    y = [1,6,6,1,1]
+    plt.xlabel("x values")
+    plt.ylabel("y values")
+    plt.plot(x,y,"b-p")
+
+small()
+medium()
+large()
+
+plt.show()
+
