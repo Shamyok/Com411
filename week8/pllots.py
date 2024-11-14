7031e4bfd51cb4dc7d3abd58d45f567f7a052a06
Sham Rai <0rais10@solent.ac.uk>
2024-11-14 13:18:09 +0000

    Added code to display a simple line plot

---
+++ week8/pllots.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+def display_line(x,y):
+    plt.plot(x,y)
+    plt.show()
+
+def run_task1():
+    x_values = [1,2,3,4,5]
+    y_values = [1,4,9,16,25]
+    display_line(x_values,y_values)
+
